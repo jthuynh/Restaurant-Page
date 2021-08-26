d0c38e465ce154d414ffb47aea76f51da2d468a8
Jennifer Huynh <jntlhuynh@gmail.com>
2021-08-25 23:09:24 -0700

    created buttons for nav and added files for modules

--- src/initPage.js
+++ src/initPage.js
@@ -1,9 +1,42 @@
+function createNav() {
+    let content = document.getElementById("content");
+
+    let nav = document.createElement("nav");
+    nav.classList.add("nav-bar");
+
+    let homeBtn = document.createElement("button");
+    homeBtn.innerHTML = "HOME";
+    let menuBtn = document.createElement("button");
+    menuBtn.innerHTML = "MENU";
+    let contactBtn = document.createElement("button");
+    contactBtn.innerHTML = "CONTACT";
+
+    homeBtn.classList.add("button-nav");
+    menuBtn.classList.add("button-nav");
+    contactBtn.classList.add("button-nav");
+
+    homeBtn.setAttribute("id","home-btn");
+    menuBtn.setAttribute("id","menu-btn");
+    contactBtn.setAttribute("id","contact-btn");
+
+    nav.append(homeBtn);
+    nav.append(menuBtn);
+    nav.append(contactBtn);
+    content.append(nav);
+}
+
 function initLoadPage() {
     let headline = document.createElement("H1");
-    let headlineText = document.createTextNode("Welcome to Samurai Sushi Boat!");
+    let headlineText = document.createTextNode("Samurai Sushi Boat");
     headline.appendChild(headlineText);
     headline.setAttribute('id','headline');
+
+    let content = document.getElementById("content");
+    console.log(content);
+    content.appendChild(headline);
     
+    createNav();
+
     let image = document.createElement("img");
     image.src = "https://storage.googleapis.com/wzukusers/user-12938003/images/57de0f7fcfba4d0Bx9j3/SUSHI1-edited-1.jpg";
     image.alt = "Combination Sashimi Plate";
@@ -26,11 +59,11 @@ function initLoadPage() {
     wonderWords.append(document.createElement("br"));
     wonderWords.append(ending);
 
-    let content = document.getElementById("content");
-    console.log(content);
-    content.appendChild(headline);
+    
     content.appendChild(image);
     content.append(wonderWords);
+
+    // Include Hours, Location
 }
 
 export default initLoadPage;
