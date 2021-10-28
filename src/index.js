cf38a0ca451324b194c72b6f5e5bf915d7aeec3f
Jennifer Huynh <jntlhuynh@gmail.com>
2021-10-27 20:42:05 -0700

    added ways to wipe content

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ initLoadPage();
 // runs correct tab module to populate it again
 function loadNewPage(e) {
     // wipe content
+
+    // clear all text and nodes with innerHTML
+    // const content = document.querySelector("content");
+    // content.innerHTML = ""
+
+    // loop to remove every last child; removes nodes
+    while(content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
     // run correct tab
 }
+
 function setupTabBrowsing() {
     const buttons = document.querySelectorAll(".nav-bar");
     console.log(buttons);
